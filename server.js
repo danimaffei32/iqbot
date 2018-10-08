babd3fdc4f0ff4e001d266734406fa6819055722
pochao <pochao@lkeng.com.tw>
2018-10-08 10:43:22 +0800

    ok

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var multer = require('multer');
 var uploadedFileName = '';
 var storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, 'public/images');
+    cb(null, 'public/images/');
   },
   filename: function(req, file, cb){
   	uploadedFileName = Date.now() + '.' + file.originalname;
