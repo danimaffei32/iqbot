dd8be60d96bc94963aeb33b8887a7ba0c4bbfc96
pochao <pochao@lkeng.com.tw>
2018-10-08 10:04:22 +0800

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var sizeOf = require('image-size');
+var multer = require('multer');
+var uploadedFileName = '';
+var storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './public/images');
+  },
+  filename: function(req, file, cb){
+  	uploadedFileName = Date.now() + '.' + file.originalname;
+    cb(null, uploadedFileName);
+  }
+})
+var upload = multer({ storage: storage });
+
+app.post('/', upload.single('upload'), (req, res, next) => {
+	//console.log(uploadedFileName);
+
+	var dimensions = sizeOf('./public/images/' + uploadedFileName);
+	//console.log(dimensions.width, dimensions.height);	
+	
+	var qrtxt = encodeURI( req.body.txt);
+
+	res.redirect('./qr.html?pic1=' + uploadedFileName + '&pic2=' + qrtxt + '&width=' + dimensions.width + '&height=' + dimensions.height);
+})
+
+app.listen(process.env.PORT || 3000);
