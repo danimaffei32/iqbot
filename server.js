dfe8421d6abcb743763b16d3244e88c73589daaa
pochao <pochao.huang@hotmail.com>
2018-10-24 10:44:39 +0800

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var multer = require('multer');
 var uploadedFileName = '';
 var storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, 'public/images/');
+    cb(null, 'public/');
   },
   filename: function(req, file, cb){
   	uploadedFileName = Date.now() + '.' + file.originalname;
@@ -24,7 +24,7 @@ var upload = multer({ storage: storage });
 app.post('/', upload.single('upload'), (req, res, next) => {
 	//console.log(uploadedFileName);
 
-	var dimensions = sizeOf('public/images/' + uploadedFileName);
+	var dimensions = sizeOf('public/' + uploadedFileName);
 	//console.log(dimensions.width, dimensions.height);	
 	
 	var qrtxt = encodeURI( req.body.txt);
@@ -32,4 +32,6 @@ app.post('/', upload.single('upload'), (req, res, next) => {
 	res.redirect('./qr.html?pic1=' + uploadedFileName + '&pic2=' + qrtxt + '&width=' + dimensions.width + '&height=' + dimensions.height);
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000,() => {
+  console.log("Server running on http://localhost:3000");
+});
